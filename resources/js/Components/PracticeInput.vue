fa684cb017a1cf285826a15b3f08fc833753fb86
Michael Claar <michael@piestar.com>
2022-02-27 21:19:42 -0600

    Added Synth Speech

--- resources/js/Components/PracticeInput.vue
+++ resources/js/Components/PracticeInput.vue
@@ -8,6 +8,9 @@
         <span class="text-gray-200" v-else @click="toggleAnswer">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye-off"><path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24"></path><line x1="1" y1="1" x2="23" y2="23"></line></svg>
         </span>
+        <span class="text-gray-200 ml-3" @click="speakAnswer">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-volume-2"><polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon><path d="M19.07 4.93a10 10 0 0 1 0 14.14M15.54 8.46a5 5 0 0 1 0 7.07"></path></svg>
+        </span>
     </div>
 </template>
 
@@ -42,7 +45,17 @@ export default {
         },
         normalizedWord(word) {
             return word.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-        }
+        },
+        speakAnswer() {
+            var synth = window.speechSynthesis;
+            let voices = synth.getVoices();
+            let utterThis = new SpeechSynthesisUtterance(this.correctAnswer);
+//            utterThis.voice = voices.find(voice => voice.name === 'Google español de Estados Unidos');
+            utterThis.voice = voices.find(voice => voice.lang === 'es-MX');
+            utterThis.pitch = 1;
+            utterThis.rate = 1;
+            synth.speak(utterThis);
+        },
     },
     computed: {
         isCorrect() {
