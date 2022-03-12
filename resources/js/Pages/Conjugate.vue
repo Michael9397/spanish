2843fa67e958c4bd789abf9f6b29f4ab17bdabdd
Michael Claar <michael@piestar.com>
2022-03-11 23:35:42 -0500

    Updated Full Conjugate Form

--- resources/js/Pages/Conjugate.vue
+++ resources/js/Pages/Conjugate.vue
@@ -1,74 +1,56 @@
 <template>
-
-    <div class="p-40 bg-gray-800 text-white">
-        <h1>Conjugate</h1>
-        <p>Currently Broken while working on the multiple view...</p>
+    <div class="p-16 bg-gray-800 text-white">
+        <h1>Conjugate Full Verb</h1>
         <div>
             <select v-model="selectedVerb" key="selectedVerb" class="bg-gray-700">
                 <option v-for="(verb, index) in conjugates" :value="index">{{ verb['infinitive'] }}</option>
             </select>
             <div v-if="selectedVerb > -1">
-                <h2 class="ml-48 font-extrabold text-3xl">Indicative</h2>
-                <div class="grid grid-cols-6">
-                    <div></div>
-                    <div v-for="tense in indicativeTense" class="font-bold ml-5">
-                        {{ `${tense[0].toUpperCase()}${tense.slice(1)}` }}
+                <h2 class="font-extrabold text-3xl">{{ conjugates[selectedVerb].infinitive.capitalize() }}</h2>
+                <div v-for="(map, tense) in conjugateMap">
+                    <h3 class="font-extrabold text-xl">{{ tense.capitalize() }}</h3>
+                    <div class="grid grid-cols-7 gap-0.5">
+                        <div></div>
+                        <div v-for="form in tenseForms(tense)">{{ form }}</div>
                     </div>
-<!--                    <div v-for="verbForm in indicativeForms">-->
-<!--                        {{ `${verbForm[0].toUpperCase()}${verbForm.slice(1)}` }}-->
-<!--                        <div v-for="tense in indicativeTense">-->
-<!--                            <input type="text" v-model="conjugates[selectedVerb][`indicative_${tense}_${verbForm}`]">-->
-<!--                        </div>-->
-<!--                    </div>-->
-                    <div class="text-right mt-4">Yo</div>
-                    <div v-for="iCase in indicativeTense">
-                        <PracticeInput :correct-answer="conjugates[selectedVerb][`indicative_${iCase}_yo`]"></PracticeInput>
-                    </div>
-                    <div class="text-right mt-4">Tu</div>
-                    <div v-for="iCase in indicativeTense">
-                        <PracticeInput :correct-answer="conjugates[selectedVerb][`indicative_${iCase}_tu`]"></PracticeInput>
-                    </div>
-                    <div class="text-right mt-4">El</div>
-                    <div v-for="iCase in indicativeTense">
-                        <PracticeInput :correct-answer="conjugates[selectedVerb][`indicative_${iCase}_el`]"></PracticeInput>
-                    </div>
-                    <div class="text-right mt-4">Nosotros</div>
-                    <div v-for="iCase in indicativeTense">
-                        <PracticeInput :correct-answer="conjugates[selectedVerb][`indicative_${iCase}_nosotros`]"></PracticeInput>
-                    </div>
-                    <div class="text-right mt-4">Vosotros</div>
-                    <div v-for="iCase in indicativeTense">
-                        <PracticeInput :correct-answer="conjugates[selectedVerb][`indicative_${iCase}_vosotros`]"></PracticeInput>
-                    </div>
-                    <div class="text-right mt-4">Ellos</div>
-                    <div v-for="iCase in indicativeTense">
-                        <PracticeInput :correct-answer="conjugates[selectedVerb][`indicative_${iCase}_ellos`]"></PracticeInput>
-                    </div>
+                    <div v-for="(elements, mode) in map">
+                        <div class="grid grid-cols-7 gap-0.5">
+                            <div class="font-extrabold">{{ mode.capitalize() }}</div>
+                            <div v-for="form in elements">
+                                <PracticeInput
+                                    :correct-answer="conjugates[selectedVerb][`${tense}_${mode}_${form}`]"
+                                    :answer-key="`${tense}_${mode}_${form}`"
+                                    :current-answer="answers[`${tense}_${mode}_${form}`]"
+                                    @change-answer="onUpdateAnswer"
+                                ></PracticeInput>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 </template>
 
-<script>
-import PracticeInput  from "@/Components/PracticeInput"
-export default {
-    name: "Conjugate",
-    props: {
+<script setup>
+    import { conjugateMap } from "@/Partials/ConjugateMap"
+    import PracticeInput  from "@/Components/PracticeInput"
+    import { ref, computed } from 'vue'
+
+    let props = defineProps({
         conjugates: {
             type: Array,
             required: true
         }
-    },
-    components: { PracticeInput},
-    data() {
-        return {
-            selectedVerb: -1,
-            indicativeTense: ['present', 'preterite', 'imperfect', 'conditional', 'future'],
-            indicativeForms: ['yo', 'tu', 'el', 'nosotros', 'vosotros', 'ellos']
-        }
-    },
-}
+    })
+    let answers = ref({})
+    let selectedVerb = ref(0)
+    let tenseForms = (tense)=> {
+        return conjugateMap[tense][Object.keys(conjugateMap[tense])[0]]
+    }
+    let onUpdateAnswer = (key, value)=> {
+        answers[key] = value
+    }
 </script>
 
 <style scoped>
